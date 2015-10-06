f442810e2c4543e0f6a2ad4c8a9af13dbfe0a2d1
Jaiprakash91 <jpthebond@gmail.com>
2015-10-06 14:54:31 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -8,11 +8,12 @@ import matplotlib.pyplot as plt
 def generate_data(n):
   data=np.zeros((n,2))
   for i in range(n):
-    data[i,0]=random(0.0,math.pi)
-    data[i,1]=math.sin(data[i,0])+ numpy.random.normal(0.0,.2,1)
+    data[i,0]=random.uniform(0.0,math.pi)
+    data[i,1]=math.sin(data[i,0])+ np.random.normal(0.0,.1,1)
   return data;
   
-  data=generate_data(20)
-  plt.plot(x[:,0],x[:,1],'ro')
-  plt.axis([-1, 4, -1, 2])
-  plt.show()
+data=generate_data(20)
+print "Data Generated."
+plt.plot(data[:,0],data[:,1],'ro')
+plt.axis([0, 4, 0, 2])
+plt.show()
